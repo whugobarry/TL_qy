8ca09a82d262f4d586f011e0837bdefe3acb44f0
whugobarry <17600740311@163.com>
2019-04-14 06:05:58 +0800

    数据库公共文件

--- admin/api.php
+++ admin/api.php
@@ -2,12 +2,15 @@
     $signature = $_GET['signature'];
     $timestamp = $_GET['timestamp'];
     $nonce = $_GET['nonce'];
+    $token = 'tlqy_20190314';
+    $echostr = $_GET['echostr'];
 
-    $tmpArr = array($timestamp,$nonce);
+    $tmpArr = array($timestamp,$nonce,$token);
     sort($tmpArr,SORT_STRING);
-    $tmpStr = implode($tmpArr);
-    if($signature){
+    $tmpStr = sha1(implode($tmpArr));
+    if($tmpStr == $signature && $echostr){
         echo true;
+        exit;
     }else{
         echo false;
     }
