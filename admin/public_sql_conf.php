edaead4c03f2f4e188a19053ced65d6807d38f2d
whugobarry <17600740311@163.com>
2019-04-14 05:51:28 +0800

    数据库公共文件

--- admin/public_sql_conf.php
+++ admin/public_sql_conf.php
@@ -3,14 +3,9 @@
     $database = new Database();
     $config = $database->database();
 
-    class Connect
-    {
-        function connect($config){
-            $conn = mysqli_connect($config['host'].':'.$config['port'],$config['user'],$config['password']);
-            mysqli_select_db($conn,$config['database']);
-            mysqli_query($conn,'set names utf8');
-        }
+    function connect($config){
+        $conn = mysqli_connect($config['host'].':'.$config['port'],$config['user'],$config['password']);
+        mysqli_select_db($conn,$config['database']);
+        mysqli_query($conn,'set names utf8');
     }
-    $connect = new Connect(); 
-    // $connect->connect($conig);
 ?>
