e4f69205288c7c2587f05727a7d3325bc1682ce7
whugobarry <17600740311@163.com>
2019-04-14 06:20:15 +0800

    数据库公共文件

--- admin/api.php
+++ admin/api.php
@@ -1,17 +1,24 @@
 <?php
-    $signature = $_GET['signature'];
-    $timestamp = $_GET['timestamp'];
-    $nonce = $_GET['nonce'];
-    $token = 'tlqy_20190314';
-    $echostr = $_GET['echostr'];
-
-    $tmpArr = array($timestamp,$nonce,$token);
-    sort($tmpArr,SORT_STRING);
-    $tmpStr = sha1(implode($tmpArr));
-    if($tmpStr == $signature && $echostr){
+    // 验证开发者服务器
+    /**
+     * 获取参数
+     * signature
+     * nonce
+     * token
+     * timestamp
+     * echostr
+     */
+    $signature = $_REQUEST['signature'];
+    $nonce = $_REQUEST['nonce'];
+    $token = 'tl_qy20190314';
+    $timestamp = $_REQUEST['timestamp'];
+    // 形成数组
+    $tmpArr = array($nonce,$timestamp,$token);
+    sort($tmpArr);
+    $str = sha1(implode($tmpArr));
+    if($str == $echostr && $signature){
+        // 第一次介入微信
         echo true;
         exit;
-    }else{
-        echo false;
     }
 ?>
